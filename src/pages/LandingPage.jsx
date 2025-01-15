b3c7110267b92fb0cca065a15af9533ccbc56bae
Ravi Kumar kushwaha <ravikushwaha2031@gmail.com>
2025-01-15 21:45:58 +0530

    changes in mobile view

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -24,7 +24,7 @@ const LandingPage = () => {
                 <Dialog>
                     <DialogTrigger asChild>
                         {/* <Button variant="outline">Edit Profile</Button> */}
-                        <CiMenuBurger className="text-3xl text-black " />
+                        <CiMenuBurger className="text-3xl text-white " />
                     </DialogTrigger>
                     <DialogContent className="sm:max-w-[425px] bg-black text-orange-600 top-44 py-20 ">
                         <DialogTitle className="text-2xl font-semibold text-center">Event Details</DialogTitle>
@@ -61,8 +61,10 @@ const LandingPage = () => {
                     </div>
                 </nav>
             </div>
+
+
             <body>
-                <div className="hero grid grid-cols-1 md:grid-cols-2 gap-4 p-4 md:px-32 bg-gradient-to-r from-black/90 via-black/90 to-orange-900 text-white font-semibold ">
+                <div className="hero py-12 grid grid-cols-1 md:grid-cols-2 gap-4 p-4 md:px-32 bg-gradient-to-r from-black/90 via-black/90 to-orange-900 text-white font-semibold ">
                     <div className="textarea">
                         <h1 className="text-6xl">Cyclothon 2025</h1>
                         <BlurText
@@ -89,7 +91,7 @@ const LandingPage = () => {
                     </div>
 
 
-                    <div className="cards">
+                    <div className="cards mt-8 md:mt-0">
                         <SpotlightCard className="custom-spotlight-card cursor-pointer " spotlightColor="rgba(0, 229, 255, 0.2)">
                             <div className="location flex gap-2">
                                 <GrLocation className="text-2xl text-orange-600" />
