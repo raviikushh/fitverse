dd4ccbe71a3bf9acbbcc7e51e9ec87d1cc13e1f9
Ravi Kumar kushwaha <ravikushwaha2031@gmail.com>
2025-01-19 22:06:49 +0530

    scroll to top..

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import MainPage from "./pages/MainPage"
 import LandingPage from './pages/LandingPage'
 import { HashRouter, Routes, Route } from 'react-router-dom'
+import ScrollToTop from './pages/ScrollToTop'
 
 function App() {
 
   return (
     <>
      <HashRouter>
+      <ScrollToTop />
       <Routes>
         <Route path='/' element={<MainPage/>}/>
         <Route path='event' element={<LandingPage/>} />
