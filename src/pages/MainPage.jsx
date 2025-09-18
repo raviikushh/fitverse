eaa05c78b025e3fe9d938fe043381f2d26e84488
Ravi Kumar kushwaha <ravikushwaha2031@gmail.com>
2025-09-18 23:38:07 +0530

    changing images to webp

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -18,11 +18,11 @@ import {
 
 
 import img1 from '/images/img1.webp'
-import img2 from '/images/img2.jpg'
-import img3 from '/images/img3.jpg'
-import img4 from '/images/img4.jpg'
-import img5 from '/images/img5.jpg'
-import img6 from '/images/img6.jpg'
+import img2 from '/images/img2.webp'
+import img3 from '/images/img3.webp'
+import img4 from '/images/img4.webp'
+import img5 from '/images/img5.webp'
+import img6 from '/images/img6.webp'
 
 
 
